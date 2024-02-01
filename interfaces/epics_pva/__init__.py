8ac3ad7f39c0f3155a1b044c634a11227dca476f
Kathryn Baker <yvd17636@athena.isis.rl.ac.uk>
2024-02-01 16:51:13 +0000

    [FIX] hot fix for machine physics in feb accounting for Enums, I/O errors and TimeoutErrors

--- interfaces/epics_pva/__init__.py
+++ interfaces/epics_pva/__init__.py
@@ -40,10 +40,12 @@ class Interface(interface.Interface):
             context.close()
 
     def get_values(self, channels: List[str]) -> Dict[str, float]:
-        time.sleep(self.poll_period)
+        # time.sleep(self.poll_period)
         context = Context("pva")
         try:
-            values = [value.raw.value for value in context.get(channels)]
+            # using real allows us to quickly extract the number from both
+            # int/floats as well as enum types
+            values = [value.real for value in context.get(channels)]
         except TimeoutError:
             logging.exception(f"Timeout error from {channels}, retrying individually")
             # if we get a timeout error one even one of them, we retry to get
@@ -51,6 +53,11 @@ class Interface(interface.Interface):
             values = [self.get_value(channel) for channel in channels]
         context.close()
         return dict(zip(channels, values))
+    def _put(self, context, value,channel):
+        try:
+            context.put(channel, value, timeout=self.timeout, get=True)
+        except TypeError:
+            context.put(channel, value.item(), timeout=self.timeout, get=True)
 
     def set_value(
         self,
@@ -64,9 +71,21 @@ class Interface(interface.Interface):
             start_time = time.time()
             # always put the value to the set PV
             try:
-                context.put(channel, value, timeout=self.timeout, get=True)
-            except TypeError:
-                context.put(channel, value.item(), timeout=self.timeout, get=True)
+                self._put(context, channel, value)
+            #     context.put(channel, value, timeout=self.timeout, get=True)
+            # except TypeError:
+            #     context.put(channel, value.item(), timeout=self.timeout, get=True)
+            except TimeoutError:
+                logging.exception(f'Timeout Error on {channel}')
+                # we try again!
+                try:
+                    self._put(context, channel, value)
+                except TimeoutError as e:
+                    # give it some time and see if it fixes itself
+                    time.sleep(1)
+                    logging.exception(f'Timeout on {channel}: {e} after 2 attempts and a 1 second delay')
+                    # raise TimeoutError(f'Timeout on {channel}: {e} after 2 tries and ')
+                # raise e
             logging.debug(f"put value {value} to {channel}")
             # then, if configured to look at a readback PV, do the check on the PV
             if set_config is not None and set_config.get("validate_readback", False):
@@ -77,18 +96,23 @@ class Interface(interface.Interface):
                 offset = set_config.get("offset", 0)
                 while count_down > 0:
                     # replace with monitor and conditional variables
-                    _value = context.get(readback_pv, timeout=self.timeout).raw.value
-                    if np.isclose(_value, value + offset, rtol=tolerance):
-                        # should we return the set value or the read value here?
-                        end_time = time.time()
-                        logging.debug(
-                            f"Set var for {channel} took {end_time - start_time:5.5f}s"
-                        )
-                        context.close()
-                        return _value
+                    _value = context.get(readback_pv, timeout=self.timeout)
+                    severity = _value.severity
+                    _value = _value.real
+                    if severity != 3:
+                        if np.isclose(_value, value + offset, atol=tolerance):
+                            # should we return the set value or the read value here?
+                            end_time = time.time()
+                            logging.debug(
+                                f"Set var for {channel} took {end_time - start_time:5.5f}s"
+                            )
+                            context.close()
+                            return _value
 
-                    time.sleep(0.1)
-                    count_down -= 0.1
+                        time.sleep(0.1)
+                        count_down -= 0.1
+                    else:
+                        logging.warning(f'readback PV {readback_pv} is in an I/O error state, validation will continue once it is out of this state')
                 logging.exception(
                     f"PV {channel} (current: {_value}) cannot reach expected value ({value}) in designated time {time_limit}!"
                 )
@@ -108,10 +132,19 @@ class Interface(interface.Interface):
         if not self.read_only:
             start = time.time()
             if self.parallel:
-                Parallel(n_jobs=mp.cpu_count())(
-                    delayed(self.set_value)(channel, value, configs.get(channel))
-                    for channel, value in zip(channels, values)
-                )
+                try:
+                    Parallel(n_jobs=mp.cpu_count())(
+                        delayed(self.set_value)(channel, value, configs.get(channel))
+                        for channel, value in zip(channels, values)
+                    )
+                except TimeoutError as e:
+                    # if we get a TimeoutError, for now we want to just continue with
+                    # the update and hope that getting the values after will calrify
+                    # what the value actually was
+                    # TODO this will have to be dealt with differently for a more 
+                    # generalisable solution across facilities as we want to make sure
+                    # that the values going into the Xopt model are correct
+                    logging.exception(e)
             else:
                 for channel, value in zip(channels, values):
                     # TODO - what should we do if we can't get to the right value?
